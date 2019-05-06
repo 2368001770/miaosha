cd2663009bfaf8cd982704b8f33d8f19555fafb2
czj <2368001770@qq.com>
2019-05-06 22:13:11 +0800

    添加sql文件

---
+++ sql/miaosha.sql
@@ -0,0 +1,142 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.6.34 : Database - miaosha
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`miaosha` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `miaosha`;
+
+/*Table structure for table `item` */
+
+DROP TABLE IF EXISTS `item`;
+
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `description` varchar(500) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `sales` int(11) NOT NULL DEFAULT '0',
+  `img_url` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `item` */
+
+insert  into `item`(`id`,`title`,`price`,`description`,`sales`,`img_url`) values (48,'小米9','2999.00','很好用的手机',7,'https://i1.mifile.cn/a1/pms_1550642182.7527088!220x220.jpg');
+
+/*Table structure for table `item_stock` */
+
+DROP TABLE IF EXISTS `item_stock`;
+
+CREATE TABLE `item_stock` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `stock` int(11) NOT NULL DEFAULT '0',
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `item_stock` */
+
+insert  into `item_stock`(`id`,`stock`,`item_id`) values (32,93,48);
+
+/*Table structure for table `order_info` */
+
+DROP TABLE IF EXISTS `order_info`;
+
+CREATE TABLE `order_info` (
+  `id` varchar(32) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  `item_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `amount` int(11) NOT NULL DEFAULT '0',
+  `order_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `promo_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `order_info` */
+
+insert  into `order_info`(`id`,`user_id`,`item_id`,`item_price`,`amount`,`order_price`,`promo_id`) values ('00000400',16,48,'2999.00',1,'2999.00',0),('00000500',16,48,'2999.00',1,'2999.00',0),('2019050400000600',16,48,'2999.00',1,'2999.00',0),('2019050500000700',16,48,'2999.00',1,'2999.00',0),('2019050500000800',16,48,'100.00',1,'100.00',3),('2019050500000900',16,48,'2999.00',1,'2999.00',0),('2019050500001000',16,48,'2999.00',1,'2999.00',0);
+
+/*Table structure for table `promo` */
+
+DROP TABLE IF EXISTS `promo`;
+
+CREATE TABLE `promo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `promo_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `start_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `end_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `item_id` int(11) NOT NULL DEFAULT '0',
+  `promo_item_price` decimal(10,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `promo` */
+
+insert  into `promo`(`id`,`promo_name`,`start_date`,`end_date`,`item_id`,`promo_item_price`) values (3,'减价大促销','2019-05-05 14:39:47','2019-05-05 14:40:01',48,'100.00');
+
+/*Table structure for table `sequence_info` */
+
+DROP TABLE IF EXISTS `sequence_info`;
+
+CREATE TABLE `sequence_info` (
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `current_value` int(11) NOT NULL DEFAULT '0',
+  `step` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `sequence_info` */
+
+insert  into `sequence_info`(`name`,`current_value`,`step`) values ('order_info',11,1);
+
+/*Table structure for table `user_info` */
+
+DROP TABLE IF EXISTS `user_info`;
+
+CREATE TABLE `user_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `gender` tinyint(4) NOT NULL DEFAULT '-1' COMMENT '1为男性，2为女性',
+  `age` int(11) NOT NULL DEFAULT '0',
+  `telphone` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `register_mode` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT 'byphone,bywechat,byalipay',
+  `third_party_id` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `telphone_unique_index` (`telphone`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `user_info` */
+
+insert  into `user_info`(`id`,`name`,`gender`,`age`,`telphone`,`register_mode`,`third_party_id`) values (10,'czj',1,22,'16735627892','byphone',''),(11,'admin',1,12,'111','byphone',''),(13,'1223',1,23,'1111','byphone',''),(15,'admin',1,29,'1234','byphone',''),(16,'陈经理',1,29,'1222','byphone','');
+
+/*Table structure for table `user_password` */
+
+DROP TABLE IF EXISTS `user_password`;
+
+CREATE TABLE `user_password` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `encrpt_password` varchar(128) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `user_password` */
+
+insert  into `user_password`(`id`,`encrpt_password`,`user_id`) values (6,'sdsdgskgjsghd',10),(7,'4QrcOUm6Wau+VuBX8g+IPg==',0),(8,'86xjyRJy8ZzpfHOXglzBXw==',0),(9,'NeLc2+oZUKeikN0MKC2goA==',15),(10,'4QrcOUm6Wau+VuBX8g+IPg==',16);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
